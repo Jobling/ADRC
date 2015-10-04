c0cdc300fea709c630ef28f5e7d17e08340d53cd
Jobling <tiago.jobling@gmail.com>
2015-10-04 19:19:50 +0100

    Criação de AddPrefix
    Commit efectuado para fins de testes.

--- Proj01/main.c
+++ Proj01/main.c
@@ -30,20 +30,50 @@ node ReadTable(char * filename){
 	// Create tree, with empty prefix (*)
 	while(aux != NULL){
 		n = sscanf(linha, "%s %d", prefix, &hop);
+		// Here we assume the prefix already comes in a binary form.
+		// Ex: 11010110\0 (8 bit)		
 		if(n != 2){
 			printf("Invalid file format\n");
 			exit(1);
 		}
-		// Update tree
+		// AddPrefix()?
 		aux = fgets(linha, BUFFSIZE, fp);
 	}
 }
 
-void AddPrefix(link root, int prefix, int hop);
-void DeletePrefix(link root, int prefix);
+void AddPrefix(link self, char * prefix, int hop){	
+	switch (prefix[0]){
+		case '\n':
+			// All out of bits
+			self->hop = hop;
+			break;
+		case '0':
+			// This bit points left
+			if (self->left == NULL){
+				self->left = (link) malloc(sizeof(node));
+			}
+			AddPrefix(self->left, prefix++, hop);
+			break;
+		case '1':
+			// This bit points right
+			if(self->right == NULL){
+				self->right = (link) malloc(sizeof(node));
+			}
+			AddPrefix(self->right, prefix++, hop);
+			break;
+		default:
+			// Not interpreted as a bit
+			printf("Unsupported prefix. Not binary.\n");
+			// There should be a memory verification here.
+			exit(1);
+			break;
+	}
+}
+
+void DeletePrefix(link root, char prefix);
 void PrintTable(link root);
 void TwoTree(link root);
-int AddressLookUp(int address);
+int AddressLookUp(char prefix);
 
 int main(int argc, char **argv){
 }
