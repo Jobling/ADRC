469ebf0c7f52d38c8b59a15e70ae3a093c8fb54a
Jobling <tiago.jobling@gmail.com>
2015-10-14 11:04:50 -0700

    Codigo comentado e optimizado (kinda?)
    Penso que a parte do codigo esteja acabada ;)

--- Proj01/main.c
+++ Proj01/main.c
@@ -28,7 +28,6 @@ Function used to print a user interface for a better understanding of available
 commands, and respective arguments, that can be applied to the tree.
 */
 void PrintInterface(){
-	printf("\n");
 	printf("-------------------------------------------------------\n");
 	printf("[COMMAND]\t- [ARGUMENTS]\n");
 	printf("-------------------------------------------------------\n");
@@ -227,73 +226,43 @@ void PrintTable(link self, char * prefix, int level){
 Function used to turn the binary tree into a 2-Tree.
 */
 void TwoTree(link self, int hop){
-	// Verify if the node is "empty"
-	if(self->hop == 0){
-		// If the node has no children it is a leaf
-		// Update next-hop with value that "came down from the tree"
-		if(self->right == NULL && self->left == NULL){
-			self->hop = hop;
-		// If the node has children, it's not a leaf
-		// Pass on the value that "came down from the tree"
-		}else{
-			// Verifying left child
-			// If it exists, "pass down value"
-			if(self->left != NULL) TwoTree(self->left, hop);
-			// If it doesn't exist, there is a right child, and this 
-			// node must have two children (2-tree definition), so
-			// we create a new child and set its value with the value
-			// that "came down the tree"
-			else{
+	// Verify if the node is "empty" or not
+	// Set value to "pass down to child nodes"
+	hop = (self->hop != 0) ? self->hop: hop;
+	
+	// If node is leaf node, it's value must be the value that was set beforehand
+	if(self->right == NULL && self->left == NULL) self->hop = hop;
+	// If the node has children, it's not a leaf
+	// Pass on the value that was set beforehand
+	else{
+		// Since the node is not a leaf, it must be "cleaned", so that
+		// only leaf nodes have values
+		self->hop = 0;
+		// Verifying left child
+		// If it exists, "pass down the value that was set beforehand"
+		if(self->left != NULL) TwoTree(self->left, hop);
+		// If it doesn't exist, there is a right child, and this 
+		// node must have two children (2-tree definition), so
+		// we create a new child and set its value with the value 
+		// that was set beforehand
+		else{
 				self->left = (link) malloc(sizeof(node));
 				self->left->right = NULL;
 				self->left->left = NULL;
 				self->left->hop = hop;
-			}
-			// Verifying right child
-			// If it exists, "pass down value"
-			if(self->right != NULL) TwoTree(self->right, hop);
-			// If it doesn't exist, there is a left child, and this 
-			// node must have two children (2-tree definition), so
-			// we create a new child and set its value with the value
-			// that "came down the tree"
-			else{
-				self->right = (link) malloc(sizeof(node));
-				self->right->right = NULL;
-				self->right->left = NULL;
-				self->right->hop = hop;
-			}
 		}
-	// If the node has a value	
-	}else{
-		// If the node has any children
-		if(self->right != NULL || self->left != NULL){
-			// Verifying left child
-			// If it exists, pass down own value
-			if(self->left != NULL) TwoTree(self->left, self->hop);
-			// If it doesn't exist, there is a right child, and this 
-			// node must have two children (2-tree definition), so
-			// we create a new child and set its value with own value
-			else{
-				self->left = (link) malloc(sizeof(node));
-				self->left->right = NULL;
-				self->left->left = NULL;
-				self->left->hop = self->hop;
-			}
-			// Verifying right child
-			// If it exists, pass down own value
-			if(self->right != NULL) TwoTree(self->right, self->hop);
-			// If it doesn't exist, there is a left child, and this 
-			// node must have two children (2-tree definition), so
-			// we create a new child and set its value with own value
-			else{
-				self->right = (link) malloc(sizeof(node));
-				self->right->right = NULL;
-				self->right->left = NULL;
-				self->right->hop = self->hop;
-			}
-			// After passing down its own value, this node (that had a value)
-			// must be "cleaned", so that only leaf nodes have values
-			self->hop = 0;
+		// Verifying right child
+		// If it exists, "pass down the value that was set beforehand"
+		if(self->right != NULL) TwoTree(self->right, hop);
+		// If it doesn't exist, there is a left child, and this 
+		// node must have two children (2-tree definition), so
+		// we create a new child and set its value with the value 
+		// that was set beforehand
+		else{
+			self->right = (link) malloc(sizeof(node));
+			self->right->right = NULL;
+			self->right->left = NULL;
+			self->right->hop = hop;
 		}
 	}
 }
