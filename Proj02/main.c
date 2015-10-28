cba860b9033a175c52be2122a91314347d9736c0
Jobling <tiago.jobling@gmail.com>
2015-10-28 19:53:27 +0000

    Definiçoes de estruturas de dados
    Isto vai ser complicado...
    Next step, ler ficheiro e criar grafo correctamente com as varias
    relacoes e imprimir tudo (para debug)

--- Proj02/main.c
+++ Proj02/main.c
@@ -1,3 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// MACROS
+/*
+Since the node ID are positive integers
+it is safe to assume negative integers as MACROS
+*/
+#define PROVIDER -1
+#define PEER -2
+#define CUSTOMER -3
+
+// Data Structures
+typedef struct node * link;
+
+/*
+Each node should contain an identifier
+A pointer to the next node (in adjency list)
+And a relationship identifier 
+*/
+struct node{
+	int id;
+	int relationship;
+	link next;
+};
+/* 
+The graph should contain information about the
+size of the network, and the relationships between
+the nodes (adjency list) 
+*/
+typedef struct{
+	int V;
+	int E;
+	list * adj;
+} * Graph;
+
+// Helper Functions
+
+// Node Functions
+
+// Statistics Functions
+
+// Main
