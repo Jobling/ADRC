0bb148b1148f3c62ab54bf8337c91143a6790e12
Jobling <tiago.jobling@gmail.com>
2015-10-29 14:28:04 +0000

    Introduçao do Edge
    Introduçao de algumas estruturas de dados e inicio da inserção no grafo.

--- Proj02/main.c
+++ Proj02/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// MACROS
+// ----------------------------------------------- MACROS ---------------------------------------
 /*
 Since the node ID are positive integers
 it is safe to assume negative integers as MACROS
@@ -13,8 +13,14 @@ it is safe to assume negative integers as MACROS
 
 #define BUFFSIZE 64
 
-// Data Structures
+// ------------------------------------------- Data Structures -------------------------------------
 typedef struct node * link;
+
+typedef struct{
+	long tail;
+	long head;
+	int relationship;
+} * Edge;
 
 /*
 Each node should contain an identifier
@@ -22,10 +28,11 @@ A pointer to the next node (in adjency list)
 And a relationship identifier 
 */
 struct node{
-	int id;
+	long id;
 	int relationship;
 	link next;
 };
+
 /* 
 The graph should contain information about the
 size of the network, and the relationships between
@@ -37,6 +44,20 @@ typedef struct{
 	list * adj;
 } * Graph;
 
+// ----------------------------------------------- Functions ---------------------------------------
+/*
+Create a new edge to be used in other functions
+*/
+Edge newE(long tail, long head, int relationship){
+	Edge e = (Edge) malloc(sizeof(*Edge));
+	
+	e->tail = tail;
+	e->head = head;
+	e->relationship = relationship;
+	
+	return e;
+}
+
 /*
 Create a new node and return it
 As this is meant to be used to create a node 'v' for 
@@ -45,9 +66,48 @@ will be the relationship 'vu'.
 So, if 'u' is a provider of 'v', the relationship shall be
 <CUSTOMER>.
 */
-link newNode(int id, int relationship, link next){
+link newNode(long id, int relationship, link next){
+	link v = (link) malloc(sizeof(node));
 	
+	v->id = id;
+	switch(relationship){
+		// 'u' is provider of 'v'/'this', so:
+		case(1):
+			v->relationship = CUSTOMER;
+			break;
+		// 'u' and 'v'/'this' are peers, so:
+		case(2):
+			v->relationship = PEER;
+			break;
+		// 'u' is a costumer of 'v'/'this' so:
+		case(3):
+			v->relationship = PROVIDER;
+			break;
+		// There was something wrong
+		default:
+			printf("Some kind of error occurred with the relationships\n")
+			v->relationship = 0;
+			break;
+	}
+	v->next = next;
+	
+	return v;
 }
+
+/*
+Insert a relationship into the graph
+Assuming no verification has been done yet.
+
+POR FAZEEEEEER
+*/
+void graphInsertE(Graph G, Edge e){
+	
+	for(i = 0; i < G->V; i++)
+	
+	
+	free(e);
+}
+
 /*
 Read the graph from a file and store it in a Graph * named G
 */
@@ -56,7 +116,8 @@ Graph readGraph(char * filename){
 	Graph G;
 	char linha[BUFFSIZE];
 	char * aux;
-	int head, tail, relationship;
+	long head, tail;
+	int	relationship;
 	
 	// Opening the file with a 'read' flag
 	if ((fp = fopen(filename, "r")) == NULL){
@@ -74,15 +135,16 @@ Graph readGraph(char * filename){
 	
 	// Node addition from file input
 	while(fgets(linha, BUFFSIZE, fp) != NULL){
-		n = sscanf(linha, "%d %d %d", &tail, &head, &relationship);
 		/*
 		Link goes from tail to head
 		If relationship == 1 => tail is provider of head
 		If relationship == 2 => tail and head are peers
 		If relationship == 3 => tail is customer of head
 		*/
-		if(n != 3) printf("Invalid line format\n");
+		if(sscanf(linha, "%li %li %d", &tail, &head, &relationship) == 3)
+			graphInsertE(G, newE(tail, head, relationship));
 		else 
+			printf("Invalid line format\n");
 	}
 	
 	// Closing file
