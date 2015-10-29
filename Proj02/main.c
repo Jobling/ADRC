dfa300ce43d21617c377c6dd299b333decc456af
Jobling <tiago.jobling@gmail.com>
2015-10-29 14:52:04 +0000

    Ja deve compilar as of graphInsertE

--- Proj02/main.c
+++ Proj02/main.c
@@ -15,12 +15,13 @@ it is safe to assume negative integers as MACROS
 
 // ------------------------------------------- Data Structures -------------------------------------
 typedef struct node * link;
+typedef struct edge * Edge;
 
-typedef struct{
+struct edge{
 	long tail;
 	long head;
 	int relationship;
-} * Edge;
+};
 
 /*
 Each node should contain an identifier
@@ -33,7 +34,7 @@ struct node{
 	link next;
 };
 
-/* 
+/*
 The graph should contain information about the
 size of the network, and the relationships between
 the nodes (adjency list) 
@@ -41,20 +42,20 @@ the nodes (adjency list)
 typedef struct{
 	int V;
 	int E;
-	list * adj;
-} * Graph;
+	link * adj;
+}Graph;
 
 // ----------------------------------------------- Functions ---------------------------------------
 /*
 Create a new edge to be used in other functions
 */
 Edge newE(long tail, long head, int relationship){
-	Edge e = (Edge) malloc(sizeof(*Edge));
-	
+	Edge e = (Edge) malloc(sizeof(struct edge));
+
 	e->tail = tail;
 	e->head = head;
 	e->relationship = relationship;
-	
+
 	return e;
 }
 
@@ -67,8 +68,8 @@ So, if 'u' is a provider of 'v', the relationship shall be
 <CUSTOMER>.
 */
 link newNode(long id, int relationship, link next){
-	link v = (link) malloc(sizeof(node));
-	
+	link v = (link) malloc(sizeof(struct node));
+
 	v->id = id;
 	switch(relationship){
 		// 'u' is provider of 'v'/'this', so:
@@ -85,12 +86,12 @@ link newNode(long id, int relationship, link next){
 			break;
 		// There was something wrong
 		default:
-			printf("Some kind of error occurred with the relationships\n")
+			printf("Some kind of error occurred with the relationships\n");
 			v->relationship = 0;
 			break;
 	}
 	v->next = next;
-	
+
 	return v;
 }
 
@@ -99,40 +100,40 @@ Insert a relationship into the graph
 Assuming no verification has been done yet.
 
 POR FAZEEEEEER
-*/
+
 void graphInsertE(Graph G, Edge e){
-	
+
 	for(i = 0; i < G->V; i++)
-	
-	
+
+
 	free(e);
 }
+*/
 
 /*
 Read the graph from a file and store it in a Graph * named G
 */
-Graph readGraph(char * filename){
+Graph * readGraph(char * filename){
 	FILE *fp;
-	Graph G;
+	Graph * G;
 	char linha[BUFFSIZE];
-	char * aux;
 	long head, tail;
-	int	relationship;
-	
+	int relationship;
+
 	// Opening the file with a 'read' flag
 	if ((fp = fopen(filename, "r")) == NULL){
 		printf("File not found\n");
 		exit(1);
 	}
-	
+
 	// Creating graph with no nodes
-	G = (Graph) malloc(sizeof(*Graph));
+	G = (Graph *) malloc(sizeof(Graph));
 	G->V = 0;
 	G->E = 0;
 	// Perguntar ao professor como saber dimensoes da lista de adjacencias
 	// OU usar realloc (em 4~5 sites so 1 e que disse 'mal')
 	G->adj = NULL;
-	
+
 	// Node addition from file input
 	while(fgets(linha, BUFFSIZE, fp) != NULL){
 		/*
@@ -143,10 +144,10 @@ Graph readGraph(char * filename){
 		*/
 		if(sscanf(linha, "%li %li %d", &tail, &head, &relationship) == 3)
 			graphInsertE(G, newE(tail, head, relationship));
-		else 
+		else
 			printf("Invalid line format\n");
 	}
-	
+
 	// Closing file
 	fclose(fp);
 	return G;
